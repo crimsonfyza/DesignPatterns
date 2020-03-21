c19cf1f391f9abd7025fd677b9796a356c338bd8
crimsonfyza <kevinmeeuwessen@outlook.com>
2020-03-21 12:19:40 +0100

    [Removed] ExamController and classes

--- src/main/java/sample/web/ui/repository/ExamRepository.java
+++
@@ -1,9 +0,0 @@
-package sample.web.ui.repository;
-
-
-import org.springframework.data.repository.CrudRepository;
-import sample.web.ui.domain.ExamObject;
-import sample.web.ui.domain.NotificationObject;
-
-public interface ExamRepository extends CrudRepository<ExamObject, Long> {
-}
