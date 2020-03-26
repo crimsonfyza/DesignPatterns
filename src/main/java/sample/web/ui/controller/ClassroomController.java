4e20bdfac034af717efe84811daf101fa18025bc
arondelooijer <aron@mbbma.nl>
2020-03-26 14:39:10 +0100

    [ADD] Classrooms listview [ADD] Classrooms form

--- src/main/java/sample/web/ui/controller/ClassroomController.java
+++ src/main/java/sample/web/ui/controller/ClassroomController.java
@@ -1,23 +1,71 @@
 package sample.web.ui.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import sample.web.ui.domain.*;
 import sample.web.ui.repository.*;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("classroom")
 public class ClassroomController {
     private final ClassroomRepository classroomRepository;
 
-    @Autowired
     public ClassroomController(ClassroomRepository classroomRepository) {
         this.classroomRepository = classroomRepository;
     }
 
-    @Autowired
+    @Transactional
+    @GetMapping
+    public ModelAndView list(){
+        Iterable<ClassroomObject> classrooms = classroomRepository.findAll();
+        return new ModelAndView("classrooms/list", "classrooms", classrooms);
+    }
+
+    @GetMapping("{id}")
+    public ModelAndView view(@PathVariable("id") ClassroomObject classroom){
+        return new ModelAndView("classrooms/view", "classroom", classroom);
+    }
+
+    @Transactional
+    @GetMapping(params = "form")
+    public ModelAndView createForm(@ModelAttribute ClassroomObject classroom){
+        return new ModelAndView("classrooms/form", "classroom", classroom);
+    }
+
+    @PostMapping
+    public ModelAndView create(@Valid ClassroomObject classroom, BindingResult result, RedirectAttributes redirect){
+        if(result.hasErrors()){
+            return new ModelAndView("classrooms/form", "formErrors", result.getAllErrors());
+        }
+
+        Classroom newClassroom;
+
+        switch (classroom.getType()){
+            case "conference":
+                newClassroom = new ConferenceClassroom();
+                break;
+
+            case "computer":
+                newClassroom = new ComputerClassroom();
+                break;
+
+            default:
+                newClassroom = new RegularClassroom();
+        }
+        saveClassroom(newClassroom, classroom.getBoard(), classroom.getName(), classroom.getCapacity());
+
+        redirect.addFlashAttribute("globalMessage", "Successfully created a new classroom");
+        return new ModelAndView("redirect:/classroom", "classroom.id", classroom.getId());
+    }
+
     public void addClassroom(){
         WhiteboardClassroomDecorator whiteboardClassroom = new WhiteboardClassroomDecorator(new ConferenceClassroom());
         classroomRepository.save(whiteboardClassroom.assignValues("AB101", 30));
@@ -27,5 +75,23 @@ public class ClassroomController {
 
         ComputerClassroom computerClassroom1 = new ComputerClassroom();
         classroomRepository.save(computerClassroom1.assignValues("AB103", 20));
+    }
+
+    public void saveClassroom(Classroom classroom, String board, String name, int capacity){
+        switch (board){
+            case "whiteboard":
+                WhiteboardClassroomDecorator whiteboardClassroom = new WhiteboardClassroomDecorator(classroom);
+                classroomRepository.save(whiteboardClassroom.assignValues(name, capacity));
+                break;
+
+            case "blackboard":
+                BlackboardClassroomDecorator blackboardClassroom = new BlackboardClassroomDecorator(classroom);
+                classroomRepository.save(blackboardClassroom.assignValues(name, capacity));
+                break;
+
+            default:
+                classroomRepository.save(classroom.assignValues(name, capacity));
+                break;
+        }
     }
 }
